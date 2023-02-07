69f9dc7357acb3d38b67867ef4c398946a79a7f0
tushar24081 <tushar24081@gmail.com>
2023-02-07 16:21:35 +0530

    Creating Update Module

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 import multer from "multer";
 import mysql2 from "mysql2"
 import bodyParser from "body-parser";
-
+const app = express();
+var jsonParser = bodyParser.json();
+app.use(bodyParser.urlencoded({extended: false}))
+app.set("view engine", "ejs");
 const connection = mysql2.createConnection({
     host: "localhost",
     user: "root",
@@ -19,9 +22,7 @@ connection.connect((err, acc) => {
 const upload = multer({dest: "upload"})
 dotenv.config();
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
     var query = `SELECT id, name FROM Student.University_Mst`;
@@ -56,10 +57,11 @@ app.post("/insert-data", upload.single("file"), (req, res) => {
 })
 
 app.get("/get-data", (req, res) => {
-    var query = "SELECT * FROM Student.Student_Mst";
+    var query = "SELECT * FROM Student.Student_Mst order by rand() LIMIT 100 ";
     connection.query(query, (err, ans) => {
         if(err) return console.log("None");
-        return res.json(ans)
+        console.log("came here");
+        res.render("data", {data: ans});
     })
 })
 app.get("/get-data/:id", (req, res) => {
@@ -67,9 +69,47 @@ app.get("/get-data/:id", (req, res) => {
     connection.query(query, (err, ans) => {
         if(err) return console.log("None");
         console.log(ans);
-        return res.json(ans);
+        res.render("data", {data: ans});
 
     })
 })
 
+app.get("/update-form/:id", (req, res) => {
+    const id = req.params.id;
+    var query = `SELECT * FROM Student.Student_Mst WHERE id = ${id}`;
+    connection.query(query, (err, ans) => {
+        if(err) return console.log("None");
+        console.log(ans);
+        // return res.json(ans);
+        console.log(ans[0].First_Name);
+        res.render("update", {data: ans, id: id})
+
+    })
+    
+})
+
+
+app.post("/update-data/:id", upload.single("file"), (req, res) => {
+    if(req.body._method === "PUT"){
+
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let cno = req.body.cno;
+    let city = req.body.city;
+    let email = req.body.email;
+    console.log(typeof(uni));
+    var query = `UPDATE Student.Student_Mst SET First_Name = '${fname}', Last_Name = '${lname}', Contact_No = '${cno}', City = '${city}', Email = '${email}' where id = ${req.params.id}`;
+
+    connection.query(query, (err, ans) => {
+        if(err) return res.send(err.message)
+        return res.send("Data Updated")
+    })
+}
+})
+
+
+
+app.delete("/delete-data/:id", (req, res) => {
+
+})
 app.listen(process.env.PORT)
