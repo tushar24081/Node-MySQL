ada3256ed30324986debb103ba0aa346bdc13ea1
tushar24081 <tushar24081@gmail.com>
2023-02-08 09:45:36 +0530

    Added Module that allow us to add Random Data into datbase

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import multer from "multer";
 import mysql2 from "mysql2"
 import bodyParser from "body-parser";
 const app = express();
+const first_name = ["Aarav", "Aryan", "Aditi", "Aishwarya", "Amit", "Amrita", "Ananya", "Anika", "Anjali", "Arjun", "Arnav", "Aryan", "Ashok", "Bharat", "Bindiya", "Chaitanya", "Chandni", "Chetan", "Darshan", "Devanshi", "Dhruv", "Diya", "Ganesh", "Garima", "Gaurav", "Gayatri", "Gitanjali", "Hari", "Hema", "Isha", "Ishan", "Jasmine", "Jayant", "Jhanvi", "Jigar", "Karan", "Kavita", "Khushi", "Kiran", "Lakshmi", "Madhuri", "Mahi", "Manav", "Meera", "Megha", "Mohit", "Mukesh", "Naveen", "Neha", "Nidhi", "Pallavi", "Parineeti", "Parth", "Pooja", "Pradeep", "Prakash", "Priya", "Rajesh", "Rajni", "Rakesh", "Raman", "Ravi", "Rekha", "Rhea", "Rohit", "Roshni", "Rupal", "Sachin", "Sakshi", "Samar", "Sarika", "Shivani", "Shivansh", "Shruti", "Shweta", "Simran", "Sohan", "Sourav", "Sridhar", "Srinivas", "Sudhir", "Sukanta", "Supriya", "Suresh", "Swathi", "Tanuja", "Tanya", "Tarun", "Tripti", "Uma", "Urvashi", "Vasudev", "Vidya", "Vikrant", "Vinay", "Vineet", "Vishal", "Vivek", "Yogesh", "Zoya"];
+
+const last_name = ["Agarwal", "Ahuja", "Arora", "Bhalla", "Bhatia", "Chauhan", "Chopra", "Das", "Datta", "Dawar", "Dhar", "Gandhi", "Gaur", "Gupta", "Jain", "Jha", "Kapoor", "Kashyap", "Kaur", "Khanna", "Kohli", "Lal", "Mehta", "Mishra", "Mitra", "Nagpal", "Nair", "Narang", "Nayyar", "Pandey", "Pant", "Parikh", "Patel", "Patil", "Rao", "Reddy", "Saha", "Sen", "Sharma", "Shukla", "Singh", "Sinha", "Tiwari", "Trivedi", "Verma", "Vohra", "Wadhwa", "Yadav", "Bose", "Chatterjee", "Dasgupta", "Ganguly", "Ghosh", "Guha", "Banerjee", "Bhattacharya", "Dey", "Dutta", "Mukherjee", "Sarkar", "Chakraborty", "Roy", "Sengupta", "Majumdar", "Bhattacharjee", "Choudhury", "Das", "Saha", "Chakravarty", "Ghoshal", "Roychoudhury", "Bhowmik", "Mukhopadhyay", "Mitra", "Banerjee", "Nag", "Saha", "Pal", "Mondal", "Kundu", "Sarkar", "Bharadwaj", "Bhatt", "Rai", "Nayar", "Menon", "Nambiar", "Nathan", "Menon", "Mohan", "Chand", "Rajan", "Nair", "Pillai", "Soman", "Nambiar", "Sivaraman", "Kurup", "Nair", "Gopinath", "Krishnan", "Vijayan", "Prakash", "Mohanan"];
+
+const tusharCities = ["Ahmedabad", "Bengaluru", "Chennai", "Delhi", "Hyderabad", "Kolkata", "Mumbai", "Pune", "Aurangabad", "Bhopal", "Chandigarh", "Coimbatore", "Faridabad", "Gurgaon", "Guwahati", "Jaipur", "Lucknow", "Nagpur", "Nashik", "Patna", "Surat", "Thane", "Vadodara", "Varanasi", "Visakhapatnam", "Amritsar", "Agra", "Aligarh", "Allahabad", "Bhubaneswar", "Calicut", "Gwalior", "Indore", "Jabalpur", "Jammu", "Kanpur", "Ludhiana", "Madurai", "Meerut", "Moradabad", "Muzaffarnagar", "Noida", "Ranchi", "Solapur", "Udaipur", "Vijayawada", "Bareilly", "Dehradun", "Jodhpur", "Kota", "Rajkot", "Shimla", "Tiruchirappalli", "Vellore", "Warangal", "Ajmer", "Akola", "Bhiwandi", "Bhavnagar", "Bokaro", "Dhanbad", "Gorakhpur", "Jhansi", "Kolhapur", "Rourkela", "Srinagar", "Thiruvananthapuram", "Ujjain"];
+
 var jsonParser = bodyParser.json();
 app.use(bodyParser.urlencoded({extended: false}))
 app.set("view engine", "ejs");
@@ -57,10 +63,9 @@ app.post("/insert-data", upload.single("file"), (req, res) => {
 })
 
 app.get("/get-data", (req, res) => {
-    var query = "SELECT * FROM Student.Student_Mst LIMIT 100 ";
+    var query = "SELECT * FROM Student.Student_Mst ORDER BY id desc LIMIT 100 ";
     connection.query(query, (err, ans) => {
         if(err) return console.log("None");
-        console.log("came here");
         res.render("data", {data: ans});
     })
 })
@@ -125,4 +130,23 @@ app.post("/search", (req, res) => {
         res.render("search", {data: ans})
     })
 })
+
+app.get("/insert-bulk", (req, res) => {
+    for (let i = 0; i < 1000; i++) {
+        let randomForName = Math.floor(Math.random() * first_name.length);
+        let randomForLastName = Math.floor(Math.random() * last_name.length);
+        let email = first_name[randomForName] + last_name[randomForLastName] + "@gmail.com"
+        let randomCity = Math.floor(Math.random() * tusharCities.length)
+        let tusharContact = Math.floor(100000000 + Math.random() * 900000000);
+        let tusharUniversity = Math.floor(Math.random() * 10) + 1;
+    
+        var query = `INSERT INTO Student.Student_Mst (First_Name, Last_Name, Contact_No, City, Email, University, createdAt) VALUES ('${first_name[randomForName]}', '${last_name[randomForLastName]}', '${tusharContact}', '${tusharCities[randomCity]}', '${email}', ${tusharUniversity}, )`;
+    
+        connection.query(query, (err, res) => {
+          if(err) return console.log(err.message);
+          
+        })
+      }
+      res.send("Data has been added successfully!")
+});
 app.listen(process.env.PORT)
